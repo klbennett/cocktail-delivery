38c5ab2c7c45bc8050404284dc3d7fc82d6c76d8
Kay Bennett <kaybennett@kbanddigital.lan>
2020-07-09 18:05:33 +0100

    Added built website

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const webpack = require("webpack");
 require("dotenv").config();
 
+const repoNameURIPrefix =
+  process.env.NODE_ENV === "production" ? "/cocktail-delivery" : "";
+
 module.exports = {
+  assetPrefix: repoNameURIPrefix,
+  env: {
+    linkPrefix: repoNameURIPrefix,
+  },
   generateBuildId: async () => "current",
   webpack: (config) => {
     const env = Object.keys(process.env).reduce((acc, curr) => {
